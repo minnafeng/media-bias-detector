af34f3e8511a7c669c841fb4f0b7699cf1dc14c5
minnafeng <minnafeng02@gmail.com>
2023-10-21 10:52:57 -0400

    refactor

--- demo/nlp_demo.py
+++ demo/nlp_demo.py
@@ -1,11 +1,11 @@
-"""Combines the major principles of natural language processing in a short, 50-line demo."""
+"""Combines the major principles of natural language processing in a short demo."""
 
 from textblob import TextBlob
 from textblob import Word
 import random
 
 
-# A Sample News Summary
+# sample news summary
 news_summary = """A woman died from treatment delays after a hospital in Germany hit by a cyberattack was forced to turn away emergency patients.
 This is a small sample of the toll from ransomware attacks, in which hackers break into computer networks and freeze the digital information until the targeted organization or city pays for its release.
 Victims have two bad choices: Give in to extortion and hope the criminals didn’t do too much damage, or refuse and risk the hackers releasing or deleting essential information.
